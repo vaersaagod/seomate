919b82cea555064988d0b2592850b131143ca136
André Elvan <andre@vaersaagod.no>
2018-06-03 22:26:48 +0200

    Removed unnecessary files.

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -18,17 +18,6 @@ use craft\web\Controller;
 /**
  * Default Controller
  *
- * Generally speaking, controllers are the middlemen between the front end of
- * the CP/website and your plugin’s services. They contain action methods which
- * handle individual tasks.
- *
- * A common pattern used throughout Craft involves a controller action gathering
- * post data, saving it on a model, passing the model off to a service, and then
- * responding to the request appropriately depending on the service method’s response.
- *
- * Action methods begin with the prefix “action”, followed by a description of what
- * the method does (for example, actionSaveIngredient()).
- *
  * https://craftcms.com/docs/plugins/controllers
  *
  * @author    Værsågod
@@ -46,7 +35,7 @@ class DefaultController extends Controller
      *         The actions must be in 'kebab-case'
      * @access protected
      */
-    protected $allowAnonymous = ['index', 'do-something'];
+    protected $allowAnonymous = true;
 
     // Public Methods
     // =========================================================================
@@ -60,19 +49,6 @@ class DefaultController extends Controller
     public function actionIndex()
     {
         $result = 'Welcome to the DefaultController actionIndex() method';
-
-        return $result;
-    }
-
-    /**
-     * Handle a request going to our plugin's actionDoSomething URL,
-     * e.g.: actions/seomate/default/do-something
-     *
-     * @return mixed
-     */
-    public function actionDoSomething()
-    {
-        $result = 'Welcome to the DefaultController actionDoSomething() method';
 
         return $result;
     }
